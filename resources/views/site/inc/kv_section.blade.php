f94ce2a370e8ce69dde8ab5afb69bd89fa2441ba
Aleksandr232 <aleksrowing232@gmail.com>
2024-06-15 07:39:51 +0300

    update

--- resources/views/site/inc/kv_section.blade.php
+++ resources/views/site/inc/kv_section.blade.php
@@ -1,14 +1,14 @@
 <section id="kv_section" class="main_section">
-    <div class="kv_bg_container">
-        <img src="{{ asset('frontend/assets/img/img_ball.png') }}" alt="" class="kv_ball">
-    </div>
-    <div class="tags_container">
-        <p class="tag_item">Футбол</p>
-        <p class="tag_item">Чемпионат</p>
-    </div>
-    <div class="kv_info_block">
-        <h1 class="kv_main_title">EURO 2024</h1>
-        <p class="kv_description">Смотрите самые захватывающие игры главного футбольного турнира Европы!</p>
-        <a href="https://premier.one/tv/matchtv?tvc=3" target="_blank" class="kv_premier_link">Смотреть</a>
-    </div>
+			<div class="kv_bg_container">
+				<img src="{{ asset('frontend/assets/img/img_ball.png') }}" alt="" class="kv_ball">
+			</div>
+			<!-- <div class="tags_container">
+				<p class="tag_item">Футбол</p>
+				<p class="tag_item">Чемпионат</p>
+			</div> -->
+			<div class="kv_info_block">
+				<h1 class="kv_main_title"><span>ЕВРО</span><span>2024</span></h1>
+				<p class="kv_description">Смотрите самые захватывающие игры главного футбольного турнира Европы!</p>
+				<a href="https://premier.one/tv/matchtv?tvc=3" target="_blank" class="kv_premier_link">Смотреть</a>
+			</div>
 </section>
