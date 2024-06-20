45a4c414ce952e89abb6c2dd027b048c0850fcfb
Aleksandr232 <aleksrowing232@gmail.com>
2024-06-20 14:42:12 +0300

    cache1800

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Cache-Control" content="max-age=3600">
+    <meta http-equiv="Cache-Control" content="max-age=1800">
 	<title>@yield('title')</title>
     <meta name="description" content="@yield('description')">
 	<link rel="stylesheet" href="{{ asset('frontend/css/style.css')}}?v=1.5.1">
