7fdf591bbaae11ddf8475d3012c392a02ab2d125
Aleksandr232 <aleksrowing232@gmail.com>
2024-07-02 09:24:32 +0300

    index

--- resources/views/admin/matchschedule/index.blade.php
+++ resources/views/admin/matchschedule/index.blade.php
@@ -223,7 +223,7 @@
                                     @endif
                 </td>
                 <td>
-                  <input value="{{$post->score}}" name="score" type="text" id="scoreInput" maxlength="12" pattern="\d+:\d+" title="Введите счет в формате 'число:число'" onkeyup="formatScoreInput(this)" />
+                  <input value="{{$post->score}}" name="score" type="text" id="scoreInput" maxlength="12" pattern="\d\(\d+\):\d\(\d+\)" title="Введите счет в формате 'число:число'" onkeyup="formatScoreInput(this)" />
                 </td>
                 <td>
                     @if($post->reference_view == NULL)
