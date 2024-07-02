745733aeb016c88e1179035e6f2354d74bf15191
Aleksandr232 <aleksrowing232@gmail.com>
2024-07-02 09:28:29 +0300

    index

--- resources/views/admin/playoff/index.blade.php
+++ resources/views/admin/playoff/index.blade.php
@@ -264,7 +264,7 @@
                     @endif
                     </td>
                     <td>
-                    <input value="{{$post->score}}" name="score" type="text" id="scoreInput" maxlength="5" pattern="\d+:\d+" title="Введите счет в формате 'число:число'" onkeyup="formatScoreInput(this)" />
+                    <input value="{{$post->score}}" name="score" type="text" id="scoreInput" maxlength="12" pattern="\d\(\d+\):\d\(\d+\)" title="Введите счет в формате 'число:число'" onkeyup="formatScoreInput(this)" />
                     </td>
                     <td><button type="submit">Подтвердить</button></td>
                 </tr>
